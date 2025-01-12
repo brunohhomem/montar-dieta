494ae6e0f05b7b9f5418d468f4063acfa79bc456
Bruno H. Homem <brunohhomem@gmail.com>
2025-01-12 11:35:36 -0300

    feat: get alimentos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <h1>hello</h1>
+import { prisma } from '../lib/prisma'
+
+export default async function Home() {
+  const alimentos = await prisma.alimento.findMany()
+
+  return (
+    <div>
+      <h1>Lista de Alimentos</h1>
+      <ul>
+        {alimentos.map(alimento => (
+          <li key={alimento.id}>
+            <strong>{alimento.desc}</strong>: {alimento.energia} kcal
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
